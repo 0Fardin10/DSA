56e1320110dcb6ae33cd46f11c2b1d5c496eab0e
0Fardin10 <fardinmd268@gmail.com>
2026-02-09 09:28:52 +0600

    basic pointer

---
+++ Linked List/Pointer/basic_pointer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int x;
+    x=10;
+    printf("%d\n",x);//output be 10 becuse it's printing x value.
+    printf("%p\n",&x);// this will be print memory location . '&' for this symbol memory address will show.
+    return 0;
+}
